89e486a59843598931e2fb6e09cee07c253872e5
Luca Wrabetz <wrabetzluca@gmail.com>
2025-02-01 21:13:53 -0500

    pytest config.

--- tests/testing.py
+++ tests/testing.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 
+from front.app import OptiFaceTUI
+
+
+def generator():
+    for i in range(10):
+        yield i
+
 
 def main():
-    print("printing...")
+    app = OptiFaceTUI()
+    app.run()
+    gen = generator()
+    for i in gen:
+        print(i)
 
 
 if __name__ == "__main__":
