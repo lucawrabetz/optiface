3c64253501981c72a66a035c9a298b499d0fc0fe
Luca Wrabetz <wrabetzluca@gmail.com>
2025-02-05 00:45:45 -0500

    Problem space - read from yaml test.

--- optiface/core/optispace.py
+++ optiface/core/optispace.py
@@ -1,30 +1,79 @@
+import os
+import yaml
+
 from dataclasses import dataclass
 from pydantic import BaseModel
-from typing import Any, Type, TypeAlias
+from typing import Any, TypeAlias, TypeVar, Generic
+
+T = TypeVar("T")
+
+_SPACE = "space"
+_PS_FILE = "problemspace.yaml"
 
 
 @dataclass
-class Feature:
+class Feature(Generic[T]):
     """
-    Struct for a problem results schema feature.
+    Struct for a (results table) schema feature.
+
+    TODO: is a feature_type: Type[T] (which requires str_to_type spines to read from yaml) useful? Starting without.
     """
 
     name: str
-    feature_type: Type
-    default: Any
+    default: T
     verbose_name: str
     short_name: str
 
     def __str__(self) -> str:
-        return f"feature: {self.name}, type: {self.feature_type}, default: {self.default}, output names: '{self.verbose_name}', '{self.short_name}'"
+        return f"feature: {self.name}, type: {type(self.default)}, default: {self.default}, output names: '{self.verbose_name}', '{self.short_name}'"
 
 
-# TODO: shoud we restrict Any to be a union of possible types? This is probably unnecessary but let's revisit...
+# 'Schema' type aliases
 GroupKey: TypeAlias = dict[str, Feature]
+
+# 'Row' type aliases
 FeatureValuePair: TypeAlias = tuple[Feature, Any]
-FeatureValueDict: TypeAlias = dict[str, FeatureValuePair]
-PathIdPair: TypeAlias = tuple[str, FeatureValueDict]
 
 
 class ProblemSpace(BaseModel):
-    pass
+    name: str
+    instance_key: GroupKey
+    solver_key: GroupKey
+    outputs: GroupKey
+    filepath: str
+
+
+def process_key(data: dict[str, Any]) -> GroupKey:
+    key: GroupKey = dict()
+    for feature_name, feature_data in data.items():
+        data_copy = feature_data
+        data_copy["name"] = feature_name
+        new_feature = Feature(**data_copy)
+        key[feature_name] = new_feature
+    return key
+
+
+def read_ps_from_yaml(name: str) -> ProblemSpace:
+    """
+    Factory for ProblemSpace:
+        - in: problem name (e.g. testproblem, knapsack)
+        - out: ProblemSpace object configured from space/<name>/problemspace.yaml
+    """
+    filepath = os.path.join(_SPACE, name, _PS_FILE)
+    instance_key: GroupKey = dict()
+    solver_key: GroupKey = dict()
+    outputs: GroupKey = dict()
+
+    with open(filepath, "r") as file:
+        yml_data = yaml.safe_load(file)
+        instance_key = process_key(yml_data["instance_key"])
+        solver_key = process_key(yml_data["solver_key"])
+        outputs = process_key(yml_data["outputs"])
+
+    return ProblemSpace(
+        name=name,
+        instance_key=instance_key,
+        solver_key=solver_key,
+        outputs=outputs,
+        filepath=filepath,
+    )
--- tests/test_optiface.py
+++ tests/test_optiface.py
@@ -1,8 +1,89 @@
 import importlib
+import os
 import pytest
 from typing import Any
 
-from optiface.core.optispace import Feature
+from optiface.core.optispace import Feature, ProblemSpace, read_ps_from_yaml
+
+TEST_PS_NAME: str = "testproblem"
+# TODO (maybe): constant below is not DRY with constants in optiface/core/optispace.py
+TEST_PS_FILEPATH: str = os.path.join("space", "testproblem", "problemspace.yaml")
+
+
+# TODO: unclear whether str is enough as solver "uuid" or if a solver id class is helpful
+def init_data_feature_set_name() -> dict[str, Any]:
+    return {
+        "name": "set_name",
+        "default": "test",
+        "verbose_name": "Set Name",
+        "short_name": "s_n",
+    }
+
+
+def set_name_string() -> str:
+    return "feature: set_name, type: <class 'str'>, default: test, output names: 'Set Name', 's_n'"
+
+
+def init_data_feature_n() -> dict[str, Any]:
+    return {
+        "name": "n",
+        "default": -1,
+        "verbose_name": "Number of Items",
+        "short_name": "n",
+    }
+
+
+def init_data_feature_rep() -> dict[str, Any]:
+    return {
+        "name": "rep",
+        "default": 0,
+        "verbose_name": "Instance Rep",
+        "short_name": "i_rep",
+    }
+
+
+def init_data_feature_solver() -> dict[str, Any]:
+    return {
+        "name": "solver",
+        "default": "MySolver",
+        "verbose_name": "Solver",
+        "short_name": "sol",
+    }
+
+
+def init_data_feature_objective() -> dict[str, Any]:
+    return {
+        "name": "objective",
+        "default": -1.0,
+        "verbose_name": "Objective",
+        "short_name": "obj",
+    }
+
+
+def init_data_feature_time_ms() -> dict[str, Any]:
+    return {
+        "name": "time_ms",
+        "default": -1.0,
+        "verbose_name": "Running Time (ms)",
+        "short_name": "t_ms",
+    }
+
+
+def init_test_problem_space() -> ProblemSpace:
+    return ProblemSpace(
+        name=TEST_PS_NAME,
+        instance_key={
+            "set_name": Feature(**init_data_feature_set_name()),
+            "n": Feature(**init_data_feature_n()),
+            "rep": Feature(**init_data_feature_rep()),
+        },
+        solver_key={"solver": Feature(**init_data_feature_solver())},
+        outputs={
+            "objective": Feature(**init_data_feature_objective()),
+            "time_ms": Feature(**init_data_feature_time_ms()),
+        },
+        filepath=TEST_PS_FILEPATH,
+    )
 
 
 class TestSetup:
@@ -16,21 +97,14 @@ class TestFeature:
     - initializes a Feature as expected
     """
 
-    DATA: dict[str, Any] = {
-        "name": "set_name",
-        "feature_type": str,
-        "default": "test",
-        "verbose_name": "Set Name",
-        "short_name": "s_n",
-    }
-
-    STRING: str = (
-        "feature: set_name, type: <class 'str'>, default: test, output names: 'Set Name', 's_n'"
-    )
-
     def test_feature_init(self):
-        set_name = Feature(**self.DATA)
-        assert str(set_name) == self.STRING
+        set_name_raw = init_data_feature_set_name()
+        set_name = Feature(**set_name_raw)
+        assert set_name.name == set_name_raw["name"]
+        assert set_name.default == set_name_raw["default"]
+        assert set_name.verbose_name == set_name_raw["verbose_name"]
+        assert set_name.short_name == set_name_raw["short_name"]
+        assert str(set_name) == set_name_string()
 
 
 class TestProblemSpace:
@@ -40,14 +114,17 @@ class TestProblemSpace:
     """
 
     def test_ps_read(self):
-        pass
+        test_ps = init_test_problem_space()
+        read_ps = read_ps_from_yaml(name=TEST_PS_NAME)
+        assert test_ps == read_ps
 
 
 class TestOptiSpace:
     """
     Behaviors:
+    - optispace initialization behaviors?
     - switch between problem spaces as expected
     """
 
     def test_ps_switch(self):
-        pass
+        assert True == False
